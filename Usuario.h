c241d7f003c06236bc8a86cf88195ea656d1ba69
VidhiSharma <vidhiav2611@gmail.com>
2023-10-10 19:35:11 +0200

    Creado Usuario (vacio)

--- Usuario.h
+++ Usuario.h
@@ -5,4 +5,77 @@
 #ifndef GRUPO3_ACT1_USUARIO_H
 #define GRUPO3_ACT1_USUARIO_H
 
+#include <list>
+#include "Libro.h"
+
+using namespace std;
+
+class Usuario {
+private:
+    string nombre;
+    string apellido;
+    string dni;
+    Libro libroActual;
+    list <Libro> historialLibro;
+
+public:
+    /**
+     * Constructor de usuario sin libroActual
+     * @param nombre
+     * @param apellido
+     * @param dni
+     */
+    Usuario(string nombre, string apellido, string dni) {
+        Usuario::nombre = nombre;
+        Usuario::apellido = apellido;
+        Usuario::dni = dni;
+    }
+
+    /**
+     * Contructor de usuario con LibroActual
+     * @param nombre
+     * @param apellido
+     * @param dni
+     * @param libroActual
+     */
+
+    Usuario(string nombre, string apellido, string dni, Libro libroActual) {
+        Usuario::nombre = nombre;
+        Usuario::apellido = apellido;
+        Usuario::dni = dni;
+        Usuario ::libroActual = libroActual;
+    }
+
+    /**
+     *
+     * @return Te devuelve el nombre
+     */
+
+    const string &getNombre() const {
+        return nombre;
+    }
+
+    const string &getApellido() const {
+        return apellido;
+    }
+
+    const string &getDni() const {
+        return dni;
+    }
+
+    const Libro &getLibroActual() const {
+        return libroActual;
+    }
+
+    void setLibroActual(const Libro &libroActual) {
+        Usuario::libroActual = libroActual;
+    }
+
+    void historialLibros() {
+        for (Libro libro: historialLibro) {
+            cout << "- " << libro.getTitulo() << endl;
+        }
+    }
+};
+
 #endif //GRUPO3_ACT1_USUARIO_H
