3f5bd334d498869a0f1dd9bf05d411794f9518ef
LuciaDoval <luciadoval1@gmail.com>
2023-10-11 13:04:50 +0200

    Libro comentado

--- Libro.h
+++ Libro.h
@@ -12,26 +12,45 @@ private:
     string categoria;
     bool disponible;
 public:
+    /**
+     * Metodo get de titulo
+     * @return te devuelve el titulo
+     */
     const string &getTitulo() const {
         return titulo;
     }
-
+    /**
+     * Metodo que cambia el titulo
+     * @param titulo
+     */
     void setTitulo(const string &titulo) {
         Libro::titulo = titulo;
     }
-
+    /**
+     * Metodo get de categoria
+     * @return te devuelve la categoria
+     */
     const string &getCategoria() const {
         return categoria;
     }
-
+    /**
+     * Metodo que cambia la categoria
+     * @param categoria
+     */
     void setCategoria(const string &categoria) {
         Libro::categoria = categoria;
     }
-
+    /**
+     * Metodo que te dice si el libro está disponible
+     * @return te dice si esta disponible
+     */
     bool isDisponible() const {
         return disponible;
     }
-
+    /**
+     * Metodo que cambia el estado de disponible
+     * @param disponible
+     */
     void setDisponible(bool disponible) {
         Libro::disponible = disponible;
     }
