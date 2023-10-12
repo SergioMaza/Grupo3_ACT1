7b9d6df48645d772c738ff12bf8efb14fa3cdb6c
SergioMaza <sergiomazahg@gmail.com>
2023-10-12 04:26:57 +0200

    Creada la funcion darLibro en el main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "Libro.h"
+#include "Usuario.h"
+using namespace std;
 
+/**
+ * Metodo para dar un Libro a un Usuario
+ * @param usuario Usuario que recibira el libro
+ * @param libro Libro que se le dara al usuario
+ */
+void darLibro(Usuario usuario, Libro libro){
+    usuario.setLibroActual(libro);
+    cout << "Le has dado [" + libro.getTitulo() + "] a " + usuario.getNombre()<< endl;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
