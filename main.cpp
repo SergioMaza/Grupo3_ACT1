bad8f368e7eb955f1bfc228aa9b0e8ac5e6076b5
Victor Bellod <victorbellod.oficial@gmail.com>
2023-10-12 12:09:47 +0200

    Creada la funcion darLibro y recogerLibro en el main

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@ void darLibro(Usuario usuario, Libro libro){
     usuario.setLibroActual(libro);
     cout << "Le has dado [" + libro.getTitulo() + "] a " + usuario.getNombre()<< endl;
 }
+/**
+ * Metodo para recoger un Libro de un Usuario
+ * @param usuario
+ * @param libro
+ */
+void recogerLibro(Usuario usuario, Libro libro){
+    usuario = Usuario(usuario.getNombre(), usuario.getApellido(), usuario.getDni());
+    libro.setDisponible(true);
+    cout << usuario.getNombre() << " ha devuelto " << libro.getTitulo() << endl;
+}
 int main() {
     return 0;
 }
